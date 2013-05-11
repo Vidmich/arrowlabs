f3f44909dbd4e3dae9d2911b2e4aebcadd5206ec
Alexei Vidmich <alexei@mint>
2013-05-11 13:00:45 -0400

    cleaning out before commiting new version

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ArrowLabs::Application.config.secret_token = '9d70ecf97f8f1ba065132da502077d8359908c0d13da2535a4d7df9bb1beb01f27a08c4f305e34e91f739b58c8c677cd0d7de1fcf7028a65a19b37570eb53d06'
--- spec/factories/models.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :contact do
-    name 'Test User'
-    email 'valid@address.com'
-    message 'Test Message\nLine2 of the test message'
-  end
-end
-
