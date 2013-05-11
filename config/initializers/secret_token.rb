0ed277d819bee7fd623f88247dfaddf973c36f45
Alexei Vidmich <alexei@vidmich.com>
2013-05-11 13:12:56 -0400

    just created a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arrowlabs::Application.config.secret_token = '735eeae6a377f451cb3c6c51a4147ee7af01144f20423d09ca3420d98aacdacb03b30f431f17cb08f761b97190a835fa572aee57e7e90489c16336d94cafa02f'
