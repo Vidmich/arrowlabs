8b59a9ccca69fd7011e3aad70d460c01261912a4
Alexei Vidmich <alexei@vidmich.com>
2013-05-17 14:43:25 -0400

    put basic content in order. implemented contact form view.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,11 @@
 class Message < ActiveRecord::Base
-  attr_accessible :body, :from, :email
+  attr_accessible  :from, :email, :body
 
   validates :from, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: { maximum: 100000 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100}, format: {with: VALID_EMAIL_REGEX}
+
+  validates :body, presence: true, length: { maximum: 100000 }
 
 end
