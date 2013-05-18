9dd57bd93ac59b4d65a1db59333cf3d31fc975fc
Alexei Vidmich <alexei@vidmich.com>
2013-05-18 13:14:28 -0400

    Finished implementing email delivery.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,21 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    if params[:commit]
+      @message = Message.new(params[:message])
+      if @message.save
+
+        MessageMailer.new_message_notification(@message).deliver
+
+        redirect_to root_url, :notice => 'Message sent successfully, we will get back to you within 24 hours'
+      else
+        flash.now[:error] = "Required fields are missing or invalid"
+        render 'welcome/index'
+      end
+    else
+      redirect_to :back
+    end
+  end
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe MessagesController do
+  before { request.env["HTTP_REFERER"] = 'coming_from_this_url' }
 
+  it 'redirects to referrer when submitted without commit parameter' do
+    post 'create'
+
+    response.should redirect_to 'coming_from_this_url'
+  end
+
+  it 'sends email when message is created ' do
+    post 'create', :message => FactoryGirl.attributes_for(:message), :commit => 'send'
+
+    email = ActionMailer::Base.deliveries.last
+    email.should_not be_nil
+    email.to.should == ['alexei@vidmich.com']
+    email.html_part.body.raw_source.should match /Message submitted/
+    email.html_part.body.raw_source.should match /Email address: test@example.com/
+    email.text_part.body.raw_source.should match /Message submitted/
+    email.text_part.body.raw_source.should match /Email address: test@example.com/
+  end
 end
